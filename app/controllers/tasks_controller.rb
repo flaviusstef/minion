99ef5481c9d2655487b1d36b5e675fdafe8465a1
Flavius Stef <flavius.stef@gmail.com>
2013-08-18 23:42:13 +0300

    Mark/unmark task as done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,4 +12,11 @@ class TasksController < ApplicationController
     Task.delete(params[:id])
     redirect_to '/tasks'
   end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.done = params[:task][:done]
+    @task.save
+    render nothing: true
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130816165154) do
+ActiveRecord::Schema.define(:version => 20130818191447) do
 
   create_table "tasks", :force => true do |t|
     t.text     "title"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.boolean  "done",       :default => false
   end
 
 end
